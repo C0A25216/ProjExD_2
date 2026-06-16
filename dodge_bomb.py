ca15a4ed7828ba1e57a2afa1fd7547f00234911f
haruto <c0a2521607@edu.teu.ac.jp>
2026-06-16 14:39:29 +0900

    練習3:画面外へ行かないようにする

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,12 +8,28 @@ WIDTH, HEIGHT = 1100, 650
 DELTA={pg.K_UP: (0,-5),pg.K_DOWN: (0,+5),pg.K_LEFT: (-5,0),pg.K_RIGHT: (+5,0)}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    引数:こうかとんRectまたは爆弾Rect
+    戻り値：横方向判定、縦方向判定
+    画面内ならTrue,画面外ならFalse
+    """
+
+    yoko,tate=True,True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko=False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+
+    #爆弾の初期化
     bb_img = pg.Surface((20, 20))        
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) 
     bb_img.set_colorkey((0, 0, 0))          
@@ -21,6 +37,7 @@ def main():
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5    
+
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
@@ -30,20 +47,29 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        
+        #跳ね返り
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  
+            vx *= -1 
+        if not tate:  
+            vy *= -1  
 
+        #こうかとん移動
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key,mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
-
+                sum_mv[1] += mv[1]        
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
         bb_rct.move_ip(vx, vy) 
         screen.blit(bb_img, bb_rct)
-        
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
